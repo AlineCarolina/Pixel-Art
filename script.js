4b044117989708e117d7e4072cb62c017388ffa7
AlineCarolina <eualine2017@gmail.com>
2021-07-09 20:55:01 -0300

    reajusta

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ paletaDeCores.addEventListener('click', function (event) {
     let corAtual = document.querySelector('.selected');
     corAtual.classList.remove('selected');
     event.target.classList.add('selected');
-    
-    
 })
+
+const pixels = document.querySelector('.linhaPixel');
+
+pixels.addEventListener('click', function (event) {
+    event.target.classList.add('pixelSelected');
+    let pintar = document.getElementsByClassName('pixelSelected');
+}) 
