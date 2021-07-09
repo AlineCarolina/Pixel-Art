b741b3e65b1638a7db3eba3aa92260052087ddfd
AlineCarolina <eualine2017@gmail.com>
2021-07-09 18:32:41 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let colorSelected = document.getElementById('black').classList.add('selected');
+const corInicial = document.getElementById('black');
+corInicial.classList.add('selected');
+const paletaDeCores = document.querySelector('#color-palette');
+
+paletaDeCores.addEventListener('click', function (event) {
+    let corAtual = document.querySelector('.selected');
+    corAtual.classList.remove('selected');
+    event.target.classList.add('selected');
+    
+    
+})
