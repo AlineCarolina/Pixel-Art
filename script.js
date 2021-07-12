59742d1a4361fe3efb77b3637ab4e63df87bc61d
AlineCarolina <eualine2017@gmail.com>
2021-07-12 20:34:35 -0300

    tentativa 92047

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const corInicial = document.getElementById('black');
 corInicial.classList.add('selected');
 const paletaDeCores = document.querySelector('#color-palette');
-
 paletaDeCores.addEventListener('click', function (event) {
-    let corAtual = document.querySelector('.selected').style.backgroundColor;
+    let corAtual = document.querySelector('.selected');
     corAtual.classList.remove('selected');
     event.target.classList.add('selected');
 })
-
 const pixelBoard = document.getElementById('pixel-board');
 
-pixelBoard.addEventListener('click', function (event) {
+pixelBoard.addEventListener('click', function(event) {
     let pixelSelecionado = event.target;
-    pixelSelecionado.classList.add('pixelSelecionado');
+    pixelSelecionado.classList.add('pixelSelecionado')
 })
